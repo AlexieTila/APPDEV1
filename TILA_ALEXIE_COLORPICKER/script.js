5be1f313b5fc470b2756bba0ff8bd1fbfcda2ff7
x <x@ca01.ub.lan>
2025-08-11 13:29:45 +0800

    Added the Color Picker Project

---
+++ TILA_ALEXIE_COLORPICKER/script.js
@@ -0,0 +1,14 @@
+const colors = ["green","red", "f15025", "yellow"]
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+
+btn.addEventListener("click" , ()=> {
+    console.log(document.body)
+    const randomNumber = getRandomNumber()
+    document.body.style.background = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
+
+getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length)
+}
